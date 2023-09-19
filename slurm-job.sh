377a27f7e2fa074fe05e5b9f3afb7ba617beeeb4
Jérôme Fink <jerome.fink@unamur.be>
2023-09-19 09:55:01 +0200

    Fixing typo

--- slurm-job.sh
+++ slurm-job.sh
@@ -19,8 +19,7 @@ source ./venv/bin/activate
 python main.py --model zhu \
  --backbone resnet34 \
  --regressor simple_roi \
- --batch_size 8 \ 
- --input_h_w 720 1280 \
+ --batch_size 8 --input_h_w 720 1280 \
  --train_sampling_stride 50 \
  --lr 5e-05 \
  --loss l1 \
