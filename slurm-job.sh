589689c233c205f8cf076c18713e19d9204faa6b
Jérôme Fink <jerome.fink@unamur.be>
2023-09-19 09:01:20 +0200

    The name of the leonardo reservation changed

--- slurm-job.sh
+++ slurm-job.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=boost_usr_prod
 #SBATCH --nodes=1
 #SBATCH --account=tra23_ELLIS
-#SBATCH --reservation s_tra_Ellis1809
+#SBATCH --reservation s_tra_Ellis
 #SBATCH --cpus-per-task=8 ### Number of threads per task (OMP threads)
 #SBATCH -o ./output/logs/%j.out
 #SBATCH -e ./output/logs/%j.err
