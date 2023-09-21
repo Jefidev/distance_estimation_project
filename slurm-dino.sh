ba949c463c11eba13e3c9b8b28662f32e74c5517
Jérôme Fink <jerome.fink@unamur.be>
2023-09-21 09:16:03 +0200

    Using value not keys

--- slurm-dino.sh
+++ slurm-dino.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=distance_estimation_dino
 #SBATCH --ntasks-per-node=1   
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:2
 #SBATCH --partition=boost_usr_prod
 #SBATCH --nodes=1
 #SBATCH --account=tra23_ELLIS
 #SBATCH --reservation s_tra_Ellis
 #SBATCH --cpus-per-task=8 ### Number of threads per task (OMP threads)
-#SBATCH -o ./output/logs/dino.out
-#SBATCH -e ./output/logs/dino.err
+#SBATCH -o ./output/logs/dino-value.out
+#SBATCH -e ./output/logs/dino-value.err
 #SBATCH --time=24:00:00
 
 module purge
@@ -28,7 +28,7 @@ python main.py --model zhu \
  --train_sampling_stride 1\
  --ds_path /leonardo/home/usertrain/a08tra51/distance_estimation_project/data/MOTSynth \
  --annotations_path /leonardo/home/usertrain/a08tra51/distance_estimation_project/annotations_clean\
- --num_gpus 4 \
+ --num_gpus 2 \
  --epochs 20
 
 
